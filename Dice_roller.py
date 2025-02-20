ae0cc90b1c8d8078398a76972d68dcddfb225473
Ch Yasir <132065602+CH-Yasir-Labs@users.noreply.github.com>
2025-02-20 19:12:32 +0500

    Add files via upload

---
+++ Dice_roller.py
@@ -0,0 +1,69 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      chyas
+#
+# Created:     11/02/2025
+# Copyright:   (c) chyas 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import random
+
+# ● ┌ ─ ┐ │ └ ┘
+
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘")
+}
+
+
+dice = []
+total = 0
+num_of_dice = int(input("How many dice?(1 to 15): "))
+
+for die in range(num_of_dice):
+    dice.append(random.randint(1, 6))
+
+# PRINT VERTICALLY
+# for die in range(num_of_dice):
+#    for line in dice_art.get(dice[die]):
+#        print(line)
+
+# PRINT HORIZONTALLY
+for line in range(5):
+    for die in dice:
+        print(dice_art.get(die)[line], end="")
+    print()
+
+for die in dice:
+    total += die
+print(f"total: {total}")
